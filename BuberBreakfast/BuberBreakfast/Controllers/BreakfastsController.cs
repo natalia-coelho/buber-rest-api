796d7c48fef8824e5bbc3905dcb8aa185e11a9b3
natalia-coelho <nataliacoelho199@gmail.com>
2023-04-15 22:34:56 -0300

    Implements service

--- BuberBreakfast/BuberBreakfast/Controllers/BreakfastsController.cs
+++ BuberBreakfast/BuberBreakfast/Controllers/BreakfastsController.cs
@@ -1,28 +1,66 @@
 using BuberBreakfast.Contracts.Breakfast;
 using Microsoft.AspNetCore.Mvc;
+using BuberBreakfast.Models;
+using BuberBreakfast.Services.Breakfasts;
 
 namespace BuberBreakfast.Controllers;
 
 [ApiController]
+[Route("[breakfasts]")]
+
 
 public class BreakfastController : ControllerBase {
-    [HttpPost("/breakfasts")]
+    
+    private readonly IBreakfastService _breakfastService;
 
-    public IActionResult CreateBreakfast(CreateBreakfastRequest request){
-        return Ok(request);
+    public BreakfastController(IBreakfastService breakfastService)
+    {
+        _breakfastService = breakfastService;
     }
 
-    [HttpGet("/breakfasts/{id:guid}")]
+    [HttpPost]
+    public IActionResult CreateBreakfast(CreateBreakfastRequest request){
+        var breakfast = new Breakfast(
+            Guid.NewGuid(),
+            request.Name,
+            request.Description,
+            request.StartDateTime,
+            request.EndDateTime,
+            DateTime.UtcNow,
+            request.Savory,
+            request.Sweet);
+
+        // TODO: save breakfast to database
+        _breakfastService.CreateBreakfast(breakfast);
+
+        var response = new BreakfastResponse(
+            breakfast.Id,
+            breakfast.Name,
+            breakfast.Description,
+            breakfast.StartDateTime,
+            breakfast.EndDateTime,
+            breakfast.LastModifiedDateTime,
+            breakfast.Savory,
+            breakfast.Sweet
+        );
+
+        return CreatedAtAction(
+            actionName: nameof(GetBreakfast),
+            routeValues: new { id = breakfast.Id },
+            value: response);
+    }
+
+    [HttpGet("{id:guid}")]
     public IActionResult GetBreakfast(Guid id){
         return Ok(id);
     }
 
-    [HttpPut("/breakfasts/{id:guid}")]
+    [HttpPut("{id:guid}")]
     public IActionResult UpsertBreakfast(Guid id, UpsertBreakfastRequest request){
         return Ok(request);
     }
 
-    [HttpDelete("/breakfasts/{id:guid}")]
+    [HttpDelete("{id:guid}")]
     public IActionResult DeleteBreakfast(Guid id){
         return Ok(id);
     }
---
+++ BuberBreakfast/BuberBreakfast/Services/Breakfasts/IBreakfastService.cs
@@ -0,0 +1,11 @@
+
+
+using BuberBreakfast.Models;
+
+    namespace BuberBreakfast.Services.Breakfasts;
+
+public interface IBreakfastService {
+
+    void CreateBreakfast (Breakfast breakfast);
+    
+}
